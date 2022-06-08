22b38a3d0bc3a850d855f11f0661a4909317a4c7
Suyog Khanal <suyogkhanal@outlook.com>
2022-06-08 09:27:22 -0400

    Change team size functionality + clean up embed

--- bot.py
+++ bot.py
@@ -8,10 +8,16 @@ intents.members = True
 client = commands.Bot(command_prefix = '.', intents=intents)
 token = 'token'
 
+team_size = 5
+
 @client.event
 async def on_ready():
     await client.change_presence(activity = discord.Game('10 Man??'))
     print('Bot is online')
+
+@client.command(aliases = ['size'])
+async def setTeamSize(ctx, *, sizeChange):
+    team_size = sizeChange
 
 @client.command(aliases = ['james', 'hashmap'])
 async def createRandom(ctx):
@@ -21,21 +27,27 @@ async def createRandom(ctx):
         members.append(i.name)
     
     x = np.array(members)
-    result = ""
+
+    result1 = ""
+    result2 = ""
 
     roles = {0 : "top", 1 : "jg", 2 : "mid", 3 : "adc", 4 : "sup"}
     np.random.shuffle(x)
 
-    for i in range(10):
-        if(i == 0):
-            result += "TEAM 1:" + "\n"
-        if(i == 5):
-            result += "\n"+ "TEAM 2:" + "\n"
-        result += str(x[i])
-        result += " " + roles[i%5]
-        result += "\n"
+    for i in range(team_size*2):
+        if(i < team_size):
+            result1 += str(x[i])
+            result1 += " " + roles[i%5]
+            result1 += "\n"
+        else:
+            result2 += str(x[i])
+            result2 += " " + roles[i%5]
+            result2 += "\n"
 
-    team_embed = discord.Embed(title = "TEAMS", description = result, color = discord.Color.red())
-    await ctx.send(embed = team_embed)
+    team1_embed = discord.Embed(title = "TEAM 1", description = result1, color = discord.Color.blue())
+    team2_embed = discord.Embed(title = "TEAM 2", description = result2, color = discord.Color.red())
+
+    await ctx.send(embed = team1_embed)
+    await ctx.send(embed = team2_embed)
 
 client.run(token)
