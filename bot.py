75a6fe57b1719213c53908d2fb0cd3607b1ce2a4
skhanal <suyogkhanal@outlook.com>
2024-06-22 20:23:07 -0400

    begin db migration to sqlite

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import random
 import discord
 from discord.ext import commands
 import numpy as np
-from pymongo import MongoClient
-import json
+import os.path as path
+import sqlite3
 
 # Set Intents
 
@@ -15,18 +15,23 @@ intents.guilds = True
 # Get token and connection string from text file
 
 token = ""
-connectionString = ""
-templatepath = ""
+
 with open("token.txt") as f:
     token = f.readline()
-    connectionString = f.readline()
-    templatepath = f.readline()
 
 # Connect to Database
 
-cluster = MongoClient(connectionString)
-db = cluster["ServerInfo"]
-collection = db["GuildData"]
+dataFolder = "data\\guildData\\serverInfo\\"  # CHANGE TO / IF ON WINDOWS MACHINE!!!!!!!!!!
+dbpath = dataFolder + "main.db"
+
+exist = path.isfile(dbpath)
+
+mainDB = sqlite3.connect(dbpath)
+cursor = mainDB.cursor()
+
+if not exist:
+    cursor.execute("CREATE TABLE servers(guildId, serverName, original_channel, result1, result2, playerString, team1, team2, players, channel1, channel2, captain1, captain2, using_captains, captainNum, drafted, team_size)")
+
 
 # Hash Map
 
